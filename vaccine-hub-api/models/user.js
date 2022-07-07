5d0d681da687cfc4934415d62d30920c2d227470
cdelira9 <cdelira@ucsd.edu>
2022-07-06 22:40:28 -0700

    step 2

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,16 @@
+const { UnauthorizedError } = require("../utils/errors");
+
+class User{
+    static async login(credentials){
+        //user submits their email and password and the email will be searched in the databse
+        //then will see if email matches the password written with the one with the database
+
+        throw new UnauthorizedError("Invalid email/password combination")
+    }
+    static async register(credentials){
+        //registering a person's email and password and hashing it to create new user 
+        //that will contain all the info in database
+    }
+}
+
+module.exports = User
--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const nodemon = require("nodemon");
-const bcrypt = require("bcrypt");
-const colors = rquire("colors");
-const dotenv = require("dotenv");
-const pg = require("pg");
+
+const { BadRequestError, NotFoundError } = require("./utils/errors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+// const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("🌋Server running http://localhost:" + PORT);
+});
